d169c12bec154034652e3ea6444768ea11574ea9
Antoine Busch <abusch@atlassian.com>
2019-02-25 18:48:47 +1100

    [fish] Add initial fish config

---
+++ shell/.config/fish/config.fish
@@ -0,0 +1,41 @@
+# fisher setup
+set -g fisher_path $HOME/.config/fish/fisher
+
+set fish_function_path $fish_function_path[1] $fisher_path/functions $fish_function_path[2..-1]
+set fish_complete_path $fish_complete_path[1] $fisher_path/completions $fish_complete_path[2..-1]
+
+for file in $fisher_path/conf.d/*.fish
+    builtin source $file 2> /dev/null
+end
+
+# abbr
+abbr -a t tig
+abbr -a ts "tig status"
+abbr -a o xdg-open
+abbr -a b bat
+
+if test -e /usr/share/fish/functions/fzf_key_bindings.fish
+    fzf_key_bindings
+end
+
+# Environment variables
+set -x RUST_SRC_PATH $HOME/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/src
+set -x FZF_DEFAULT_COMMAND 'fd --type f --follow'
+set -x FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
+
+# Base16 Shell
+if status --is-interactive
+    set BASE16_SHELL "$HOME/.config/base16-shell/"
+    source "$BASE16_SHELL/profile_helper.fish"
+end
+
+# colored man output
+# from http://linuxtidbits.wordpress.com/2009/03/23/less-colors-for-man-pages/
+set -x LESS_TERMCAP_mb \e'[01;31m'       # begin blinking
+set -x LESS_TERMCAP_md \e'[01;38;5;74m'  # begin bold
+set -x LESS_TERMCAP_me \e'[0m'           # end mode
+set -x LESS_TERMCAP_se \e'[0m'           # end standout-mode
+set -x LESS_TERMCAP_so \e'[38;5;246m'    # begin standout-mode - info box
+set -x LESS_TERMCAP_ue \e'[0m'           # end underline
+set -x LESS_TERMCAP_us \e'[04;38;5;146m' # begin underline
+
