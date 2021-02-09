75028efc936993d78c4ff749cbb36fee853ad994
Antoine Busch <antoine.busch@gmail.com>
2021-02-10 01:12:48 +1100

    [fish] remove fisher

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,14 +1,4 @@
-# fisher setup
-set -g fisher_path $HOME/.config/fish/fisher
-
-set fish_function_path $fish_function_path[1] $fisher_path/functions $fish_function_path[2..-1]
-set fish_complete_path $fish_complete_path[1] $fisher_path/completions $fish_complete_path[2..-1]
-
-for file in $fisher_path/conf.d/*.fish
-    builtin source $file 2> /dev/null
-end
-
-set -gx PATH $PATH $HOME/.cargo/bin
+set -gx PATH $HOME/.local/bin $HOME/.cargo/bin $PATH
 
 # abbr
 abbr -a t tig
